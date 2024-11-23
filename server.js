ab5034449a9f3f6364de070ad0de223a428f4799
John Mark Manuel <manueljm016@gmail.com>
2024-11-23 16:22:33 +0800

    Cors configured

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ const routes = require("./routes/");
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Z-key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+  });
 
 app.use("/", routes);
 
