5cd2af6d94208d43c0f2768cf2ceea21a5d75bda
John Mark Manuel <manueljm016@gmail.com>
2024-11-30 08:28:14 +0800

    Oauth added

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,24 @@ const express = require("express")
 const router = new express.Router();
 const employees = require('./employees');
 const swaggerRoute = require('./swagger');
+const passport = require('passport');
 
 router.use('/', swaggerRoute);
+/*
 router.get('/', (req, res) => {
     //#swagger.tags=['Hello World']
     res.send('Hello World');
 });
+*/
 router.use('/employees', employees);
+
+router.get("/login", passport.authenticate('github'), (req, res) => {});
+
+router.get("/logout", function(req, res, next) {
+	req.logout(function(err) {
+		if (err) return next(err);
+		res.redirect('/')
+	})
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,20 +6,65 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 env.config();
 const routes = require("./routes/");
+const passport = require("passport");
+const session = require("express-session");
+const GitHubStrategy = require("passport-github2").Strategy;
 
-app.use(bodyParser.json());
-app.use(cors());
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Z-key'
-    );
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    next();
-  });
+app
+	.use(bodyParser.json())
+	.use(session({
+		secret: "secret",
+		resave: false,
+		saveUninitialized: true
+	}))
+	// This is the basic express session({...}) initialization
+	.use(passport.initialize())
+	// init passport in every route call.
+    .use(passport.session())
+	// Init passport to use "express-session"
+    .use((req, res, next) => {
+		res.setHeader("Access-Control-Allow-Origin", "*");
+		res.setHeader(
+			"Access-Control-Allow-Headers",
+			"Origin, X-Requested-With, Content-Type, Accept, Z-Key, Authorization"	
+		);
+		res.setHeader(
+			"Access-Control-Allow-Methods",
+			"POST, GET, PUT, PATCH, OPTIONS, DELETE"
+		);
+		next();
+	})
+	.use(cors({methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"]}))
+	.use(cors({origin: "*"}))
+	.use("/", routes);
 
-app.use("/", routes);
+passport.use(new GitHubStrategy({
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: process.env.CALLBACK_URL
+    },
+    function(accessToken, refreshToken, profile, done) {
+        //User.findOrCreate({ githubId: profile.id }, function (err, user) {
+            return done(null, profile);
+        //})
+    }
+));
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
+app.get("/", (req, res) => {res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : "Logged Out")});
+
+app.get('/github/callback', passport.authenticate('github', {
+	failureRedirect: '/api-docs', session: false}),
+	(req, res) => {
+	req.session.user = req.user;
+	res.redirect("/");
+});
 
 process.on('uncaughtException', (err, origin) => {
     console.log(process.stderr.fd, `Caught Exception: ${err}/n`,  `Exception Origin: ${origin};`);
